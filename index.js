8d5ceeadbce73849860280679b5136c5c4b0d82f
Scott Silverman <scottms724@gmail.com>
2018-01-25 19:58:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function iLoveTheBeatles(number) {
     loveBeatles.push("I love the Beatles!");
     number++;
   } while(number<15)
-    loveBeatles.push("I love the Beatles!");
   return loveBeatles;
 }
 
